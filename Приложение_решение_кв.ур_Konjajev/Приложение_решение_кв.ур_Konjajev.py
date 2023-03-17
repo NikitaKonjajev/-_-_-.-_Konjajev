cefac31bf4bfb88c7634c2ee2c559b4d582ce903
Никита <@DESKTOP-HK185OH>
2023-03-17 19:26:38 +0200

    готовая работа

--- Приложение_решение_кв.ур_Konjajev/Приложение_решение_кв.ур_Konjajev.py
+++ Приложение_решение_кв.ур_Konjajev/Приложение_решение_кв.ур_Konjajev.py
@@ -5,39 +5,80 @@ aken=Tk()
 aken.geometry("600x200")
 aken.title(tekst)
 
-nupp=Button(aken, 
+pealkiri=Label(aken, #текст сверху "Решение квадратного уравнения"
+               text="Решение квадратного уравнения",
+               bg="lightblue",
+               fg="green",
+               font="Algerian 20",
+               height=1,
+               width=50,
+               justify=RIGHT)
+
+nupp=Button(aken, #кнопка решить 
             text="Решить",
             bg="green",
-            fg="#0d010b",
+            fg="black",
             font="Chiller 20",
             activebackground="red",
             activeforeground="white",
             height=2,
-            width=15)
+            width=10)
 
-pealkiri=Label(aken,
-               text="Решение квадратного уравнения",
-               bg="lightblue",
+nupp2=Button(aken, #кнопка решение
+            text="Решение",
+            bg="yellow",
+            fg="black",
+            font="Algerian 10",
+            height=3,
+            width=52)
+
+pealkiri1=Label(aken, #пишет x**2
+               text="x**2+",
                fg="green",
-               font="Chiller 20",
-               height=1,
-               width=50,
-               justify=RIGHT)
-tekst_kast=Entry(aken,
+               font="Algerian 22", 
+               height=5,
+               width=5)
+
+pealkiri2=Label(aken, #пишет x+
+               text="x+",
+               fg="green",
+               font="Algerian 22", #Algerian
+               height=2,
+               width=2)
+
+pealkiri3=Label(aken, #Пишет =0
+               text="=0",
+               fg="green",
+               font="Algerian 22", #Algerian
+               height=2,
+               width=2)
+
+tekst_kast=Entry(aken, #окно куда писть 1
                  fg="Lightblue",
                  bg="Lightblue",
-                 font="Chiller 20",
+                 font="Algerian 20",
                  width=4)
 
-tekst_kast1=Entry(aken,
+tekst_kast1=Entry(aken, #окно куда писать 2
                  fg="Lightblue",
                  bg="Lightblue",
-                 font="Chiller 20",
+                 font="Algerian 20",
                  width=4)
 
+tekst_kast2=Entry(aken, #окно куда писать 3
+                 fg="Lightblue",
+                 bg="Lightblue",
+                 font="Algerian 20",
+                 width=3,
+                 justify=LEFT)
 
 pealkiri.pack() 
-tekst_kast.pack(pady=40,side=LEFT)
-tekst_kast.pack(pady=40,side=LEFT)
-nupp.pack(anchor=E)
+nupp2.pack(side=BOTTOM)
+tekst_kast.pack(side=LEFT)
+pealkiri1.pack(side=LEFT)
+tekst_kast1.pack(side=LEFT)
+pealkiri2.pack(side=LEFT)
+tekst_kast2.pack(side=LEFT)
+pealkiri3.pack(side=LEFT)
+nupp.pack(side=LEFT)
 aken.mainloop()
