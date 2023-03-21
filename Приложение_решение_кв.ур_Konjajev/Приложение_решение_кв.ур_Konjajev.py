d6d804ef99691e2ed5544b250be3f02d4bfef7b5
opilane <opilane@HP-CZC2349HV1>
2023-03-21 15:40:52 +0200

    123

--- Приложение_решение_кв.ур_Konjajev/Приложение_решение_кв.ур_Konjajev.py
+++ Приложение_решение_кв.ур_Konjajev/Приложение_решение_кв.ур_Konjajev.py
@@ -92,10 +92,10 @@ colors=["black",
 raam2=Tk()
 raam2.title("Ringid")
 tahvel2=Canvas(raam2,width=600,height=600,bg="white")
-x=0
+x0=0
 y0=0
 x1=600
-y2=600
+y1=600
 p=5
 for i in range(55):
     x0+=p 
